1cb7f46b1f5b0a5c72f84fae83ddbd1583df5b34
renefiedel <renefiedel@outlook.com>
2020-10-08 18:55:10 +0200

    8th october

--- ism/mainIsm.py
+++ ism/mainIsm.py
@@ -5,7 +5,7 @@ from ism.src.ism import ism
 
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = '/home/renefiedel/Documents/EO DATA PROCESSING/EODP/EODP/auxiliary'
-indir = '/home/renefiedel/Documents/EO DATA PROCESSING/EODP_TER-20200917T082333Z-001/EODP_TER/EODP-TS-ISM/input'
+indir = '/home/renefiedel/Documents/EO DATA PROCESSING/EODP_TER-20200917T082333Z-001/EODP_TER/EODP-TS-ISM/input/gradient_alt100_act150'
 outdir = '/home/renefiedel/Documents/EO DATA PROCESSING/EODP/EODP/Output/ism_output'
 
 # Initialise the ISM
--- ism/new project.py
+++
@@ -1,1 +0,0 @@
-print("EO data processing.")
