5877fe310418b9be1c556e7c8b7564713196ac58
renefiedel <renefiedel@outlook.com>
2020-11-06 12:24:51 +0100

    LIC test debug

--- l1c/ut01/output/test_L1C.py
+++ l1c/ut01/output/test_L1C.py
@@ -4,12 +4,17 @@ from common.io.writeToa import readToa
 
 
 indir = '/home/renefiedel/Documents/EO DATA PROCESSING/EODP/EODP/Output/l1c_output'
-mytoa = readToa(indir, "l1c_toa_VNIR-0.nc")
-l1c1 = np.sort(mytoa)
-
 refoutdir = '/home/renefiedel/Documents/EO DATA PROCESSING/EODP_TER-20200917T082333Z-001/EODP_TER/EODP-TS-L1C/output'
-reftoa = readToa(refoutdir, "l1c_toa_VNIR-0.nc")
-l1c2 = np.sort(reftoa)
 
-difftoa = np.min(np.abs(l1c1-l1c2))
-print(difftoa)
+mytoa = [None]*4
+reftoa = [None]*4
+diff_toa = [None]*4
+
+for ii in range(4):
+    mytoa[ii] = readToa(indir, "l1c_toa_VNIR-" + str(ii) + ".nc")
+    reftoa[ii] = readToa(refoutdir, 'l1c_toa_VNIR-' + str(ii) + '.nc')
+    mytoa[ii] = np.sort(mytoa[ii])
+    reftoa[ii] = np.sort(reftoa[ii])
+
+    diff_toa[ii] = np.min(np.abs((mytoa[ii] - reftoa[ii])/reftoa[ii]))
+    print('difftoa_VNIR-' + str(ii) + '=', diff_toa[ii]*100)
